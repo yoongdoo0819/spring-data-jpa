bc542d112498430a98bb60f9c6552d785827e10d
yoongdoo <yoong.n@krustuniverse.com>
2023-04-13 18:27:45 +0900

    add jpa-based repo (not spring-data)

--- src/main/java/study/datajpa/repository/TeamRepository.java
+++ src/main/java/study/datajpa/repository/TeamRepository.java
@@ -2,6 +2,7 @@ package study.datajpa.repository;
 
 import org.springframework.stereotype.Repository;
 import study.datajpa.entity.Member;
+import study.datajpa.entity.Team;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
@@ -9,22 +10,22 @@ import java.util.List;
 import java.util.Optional;
 
 @Repository
-public class MemberJpaRepository {
+public class TeamRepository {
 
     @PersistenceContext
     private EntityManager em;
 
-    public Member save(Member member) {
-        em.persist(member);
-        return member;
+    public Team save(Team team) {
+        em.persist(team);
+        return team;
     }
 
-    public void delete(Member member) {
-        em.remove(member);
+    public void delete(Team team) {
+        em.remove(team);
     }
 
-    public List<Member> findALl() {
-        return em.createQuery("select m from Member m", Member.class)
+    public List<Team> findAll() {
+        return em.createQuery("select t from Team t", Team.class)
                 .getResultList();
     }
 
@@ -34,11 +35,8 @@ public class MemberJpaRepository {
     }
 
     public long count() {
-        return em.createQuery("select count(m) from Member m", Long.class)
+        return em.createQuery("select count(t) from Team t", Long.class)
                 .getSingleResult();
     }
 
-    public Member find(Long id) {
-        return em.find(Member.class, id);
-    }
 }
--- src/test/java/study/datajpa/repository/MemberRepositoryTest.java
+++ src/test/java/study/datajpa/repository/MemberRepositoryTest.java
@@ -17,16 +17,40 @@ import static org.junit.jupiter.api.Assertions.*;
 @Commit
 class MemberRepositoryTest {
 
-    @Autowired MemberRepository memberRepository;
+    @Autowired MemberJpaRepository memberJpaRepository;
 
     @Test
     public void testMember() {
         Member member = new Member("memberA");
-        Member savedMember = memberRepository.save(member);
+        Member savedMember = memberJpaRepository.save(member);
 
-        Member findMember = memberRepository.findById(savedMember.getId()).get();
+        Member findMember = memberJpaRepository.findById(savedMember.getId()).get();
 
         assertThat(findMember.getId()).isEqualTo(member.getId());
         assertThat(findMember.getUsername()).isEqualTo(member.getUsername());
+    }
+
+    @Test
+    public void basicCRUD() {
+        Member member1 = new Member("member1");
+        Member member2 = new Member("member2");
+
+        memberJpaRepository.save(member1);
+        memberJpaRepository.save(member2);
+
+        Member findMember1 = memberJpaRepository.findById(member1.getId()).get();
+        Member findMember2 = memberJpaRepository.findById(member2.getId()).get();
+
+        assertThat(findMember1).isEqualTo(member1);
+        assertThat(findMember2).isEqualTo(member2);
+
+        long count = memberJpaRepository.count();
+        assertThat(count).isEqualTo(2);
+
+        memberJpaRepository.delete(member1);
+        memberJpaRepository.delete(member2);
+
+        long deletedCount = memberJpaRepository.count();
+        assertThat(deletedCount).isEqualTo(0);
     }
 }
